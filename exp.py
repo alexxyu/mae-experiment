b3a232f1dacf63cb5a11d9e51b444841a3dccc1d
alexxyu <yu.alexanderx@gmail.com>
2020-03-26 20:37:33 -0700

    Grating sizing

--- exp.py
+++ exp.py
@@ -3,17 +3,21 @@ import random
 import numpy as np
 import pandas as pd
 import configparser
-from psychopy import visual, event, core, logging, gui, sound
+from psychopy import visual, event, core, logging, gui, sound, monitors
+
+parser = configparser.ConfigParser()
+parser.read('config.ini')
 
 # EXPERIMENTAL SETTINGS
-trials_per_speed = 20
+TRIALS_PER_SPEED = int(parser['ExperimentOptions']['TrialsPerSpeed'])
+FIXATOR_SIZE_PX = int(parser['ExperimentOptions']['FixatorSizePx'])
 
-INIT_NO_ADAPTION_TIME = 30
-INIT_ADAPTION_TIME = 120
-POST_NO_ADAPTION_TIME = 5
-POST_ADAPTION_TIME = 5
-TEST_STIMULUS_TIME = 0.075
-ISI_TIME = 0.5
+INIT_NO_ADAPTION_TIME = float(parser['ExperimentOptions']['InitialNoAdaptionTime'])
+INIT_ADAPTION_TIME = float(parser['ExperimentOptions']['InitialAdaptionTime'])
+POST_NO_ADAPTION_TIME = float(parser['ExperimentOptions']['PostNoAdaptionTime'])
+POST_ADAPTION_TIME = float(parser['ExperimentOptions']['PostAdaptionTime'])
+TEST_STIMULUS_TIME = float(parser['ExperimentOptions']['TestStimulusTime'])
+ISI_TIME = float(parser['ExperimentOptions']['ISITime'])
 
 # GRAPHICAL OPTIONS
 BG_COLOR = "#636363"
@@ -23,8 +27,6 @@ key_list = ['right', 'left']
 data_columns = ['Response', 'Time']
 
 # Global variables
-parser = configparser.ConfigParser()
-parser.read('config.ini')
 window = None
 fixator = None
 prompt = None
@@ -35,26 +37,41 @@ def main():
 
     # Prompt GUI for experimental setup
     dlg = gui.Dlg(title='Experiment Setup')
-    dlg.addText('Participant Info:')
-    dlg.addField('ID:')                                                                     # res[0]
-    dlg.addField('Age:')                                                                    # res[1]
-    dlg.addField('Gender:', choices=['M', 'F'])                                             # res[2]
-    dlg.addText('Experiment Setup:')
+    dlg.addField('Participant ID:')                                                         # res[0]
+    dlg.addField('Participant Age:')                                                        # res[1]
+    dlg.addField('Participant Gender:', choices=['M', 'F'])                                 # res[2]
     dlg.addField('Experimenter ID:')                                                        # res[3]
     dlg.addField('Include Adaption:', 'Both', choices=['Both', 'No', 'Yes'])                # res[4]
     dlg.addField('Adaption Direction:', 'Random', choices=['Random', 'Clockwise', 'Counterclockwise'])      # res[5]
     res = dlg.show()
     if not dlg.OK:
         exit()
-    exp_bg_info = res[:4]
 
+    dlg = gui.Dlg(title='Monitor Setup')
+    dlg.addText('Monitor Info:')
+    dlg.addField('Viewing distance (cm):', 57)
+    dlg.addField('Width (cm):')
+    dlg.addField('Width (pixels):')
+    dlg.addField('Height (pixels):')
+    monitor_info = dlg.show()
+    if not dlg.OK:
+        exit()
+    viewing_dist, width_cm, width_px, height_px = [int(n) for n in monitor_info]
+
+    exp_bg_info = res[:4]
     to_run_no_adaption = True if res[4] != 'Yes' else False
     to_run_adaption = True if res[4] != 'No' else False
     adaption_dir = res[5]
 
     #TODO: Gamma correction
-    window = visual.Window(size=(600,600), color=BG_COLOR, monitor='monitor', fullscr=False)    
-    fixator = visual.Circle(window, size=(5, 5), units='pix')
+    monitor = monitors.Monitor('monitor')
+    monitor.setSizePix((height_px, width_px))
+    monitor.setWidth(width_cm)
+    monitor.setDistance(viewing_dist)
+    window = visual.Window(color=BG_COLOR, monitor=monitor, fullscr=True) 
+
+    refresh_rate = window.getActualFrameRate()   
+    fixator = visual.Circle(window, size=(FIXATOR_SIZE_PX, FIXATOR_SIZE_PX), units='pix')
 
     if to_run_no_adaption:
         run_without_adaption(window)
@@ -78,20 +95,20 @@ def run_without_adaption(window):
     prompt.draw()
     window.flip()
     event.waitKeys()
-    prompt.text = 'The test will be a rotating grating. Press the left arrow key if it appears to be moving clockwise and the right arrow key if it appears to be moving counterclockwise.\n\nAfter the grating, you''ll see a rotating grating again for 5 seconds, and then a beep and test image.\n\nThis will repeat until the end of the experiment.\n\nPress any key to start.'
+    prompt.text = 'The test will be a rotating grating. Press the left arrow key if it appears to be moving clockwise and the right arrow key if it appears to be moving counterclockwise.\n\nAfter the grating, you''ll see a blank screen again for 5 seconds, and then a beep and test image.\n\nThis will repeat until the end of the experiment.\n\nPress any key to start.'
     prompt.draw()
     window.flip()
     event.waitKeys()
 
-    #TODO: Stimulus sizing
-    stim = visual.RadialStim(window, tex='sin', size=(200, 200), units='pix', angularCycles=int(parser['GratingOptions']['GratingCycles']))
+    grating_size = int(parser['GratingOptions']['GratingSize'])
+    stim = visual.RadialStim(window, tex='sin', size=(grating_size, grating_size), units='deg', angularCycles=int(parser['GratingOptions']['GratingCycles']))
 
     # Create and loop through trials
     trials = []
     min_speed = float(parser['NoAdaptionOptions']['MinTestSpeed'])
     max_speed = float(parser['NoAdaptionOptions']['MaxTestSpeed'])
     step_size = float(parser['NoAdaptionOptions']['StepSize'])
-    for i in range(trials_per_speed):
+    for _ in range(TRIALS_PER_SPEED):
         temp = [round(n, 5) for n in np.arange(min_speed, max_speed + step_size, step=step_size)]
         trials.extend(temp)
     random.shuffle(trials)
@@ -144,17 +161,17 @@ def run_with_adaption(window, adaption_dir):
     data = pd.DataFrame(columns=data_columns)
 
     # Experimental instructions
-    prompt = visual.TextStim(window, color='white', text='You will see a blank screen for 30 seconds. You do not have to press anything. Try to keep your eyes fixed during this time.\n\nThen, you will hear a beep to alert you that a test is about to begin.\n\nPress any key to continue.')
+    prompt = visual.TextStim(window, color='white', text='You will first see a rotating grating for 2 minutes. Try to keep your eyes fixed on the grating during this time.\n\nPress any key to continue.')
     prompt.draw()
     window.flip()
     event.waitKeys()
-    prompt.text = 'The test will be a rotating grating. Press the left arrow key if it appears to be moving clockwise and the right arrow key if it appears to be moving counterclockwise.\n\nAfter the grating, you''ll see a rotating grating again for 5 seconds, and then a beep and test image.\n\nThis will repeat until the end of the experiment.\n\nPress any key to start.'
+    prompt.text = 'The test will be a rotating grating. Press the left arrow key if it appears to be moving clockwise and the right arrow key if it appears to be moving counterclockwise.\n\nAfter the grating, you''ll see a rotating grating again for 5 seconds, and then a beep and test grating.\n\nThis will repeat until the end of the experiment.\n\nPress any key to start.'
     prompt.draw()
     window.flip()
     event.waitKeys()
 
-    #TODO: Stimulus sizing
-    stim = visual.RadialStim(window, tex='sin', size=(200, 200), units='pix', angularCycles=int(parser['GratingOptions']['GratingCycles']))
+    grating_size = int(parser['GratingOptions']['GratingSize'])
+    stim = visual.RadialStim(window, tex='sin', size=(grating_size, grating_size), units='deg', angularCycles=int(parser['GratingOptions']['GratingCycles']))
 
     if adaption_dir == 'Random':
         adaption_dir = 'Clockwise' if random.getrandbits(1) == 0 else 'Counterclockwise'
@@ -168,7 +185,7 @@ def run_with_adaption(window, adaption_dir):
         step_size = float(parser['CCWAdaptionOptions']['StepSize'])
 
     trials = []
-    for i in range(trials_per_speed):
+    for _ in range(TRIALS_PER_SPEED):
         temp = [round(n, 5) for n in np.arange(min_speed, max_speed + step_size, step=step_size)]
         trials.extend(temp)
     random.shuffle(trials)
